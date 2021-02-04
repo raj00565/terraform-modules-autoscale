754b17afb8a03d20c771b65d98c9d7eef2cd0499
Raj Kumar <raj00565@gmail.com>
2021-02-05 02:18:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,10 +7,16 @@ variable "cluster_name" {
   description = "The name to use for all the cluster resources"
   type        = string
 }
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
 
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket for the database's remote state"
   type        = string
+  default    = autoscale_region
 }
 
 variable "db_remote_state_key" {
